88d20876c71e8fb43c9f431332a6f94be073ea5b
vaish933 <147526562+vaish933@users.noreply.github.com>
2024-07-08 23:56:37 +0530

    first commit

--- app.py
+++ app.py
@@ -2,20 +2,21 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 
-popular_df = pickle.load(open('popular.pkl','rb'))
+popular_df=pickle.load(open('popular.pkl','rb'))
 pt=pickle.load(open('pt.pkl','rb'))
 books=pickle.load(open('books.pkl','rb'))
 similarity_score=pickle.load(open('similarity_score.pkl','rb'))
+
 app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html',
-                           book_name = list(popular_df['Book-Title'].values),
-    author = list(popular_df['Book-Author'].values),
-    image = list(popular_df['Image-URL-M'].values),
-    votes = list(popular_df['num_rating'].values),
-    rating = list(popular_df['avg_rating'].values))
-
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num_rating'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           )
 @app.route('/recommend')
 def recommend_ui():
     return render_template('recommend.html')
@@ -25,7 +26,6 @@ def recommend():
     user_input=request.form.get('user_input')
     index = np.where(pt.index == user_input)[0][0]
     similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x: x[1], reverse=True)[1:5]
-
     data = []
     for i in similar_items:
         item = []
@@ -36,6 +36,6 @@ def recommend():
         data.append(item)
     print(data)
 
-    return render_template('recommend.html',data=data)
+    return render_template('recommend.html', data=data)
 if __name__=='__main__':
     app.run(debug=True)
